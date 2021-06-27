7f00bde8d9adbd2fc5b702c87e6f42707795cf15
Bradley <bmcguigandev@outlook.com>
2021-06-27 01:18:00 +0100

    Allow user to delete projects

--- src/display/initialize.js
+++ src/display/initialize.js
@@ -3,10 +3,13 @@ import main from "./main";
 import TodoProjectsController from "../todoProjectsController";
 import { createAccordion, createAccordionItem } from "./accordion";
 import TodoProject from "../todoProject";
+import ProjectsController from "../todoProjectsController";
 
 function initializeLayout() {
     document.body.appendChild(navbar);
     document.body.appendChild(main);
+    const projectDisplay = document.querySelector("#project-display");
+    setProjectDisplayDefault(projectDisplay);
 }
 
 function populateProjectsList() {
@@ -15,16 +18,51 @@ function populateProjectsList() {
 
     const projects = TodoProjectsController.getProjects();
     projects.forEach(project => {
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("btn", "btn-danger", "btn-sm");
+        deleteButton.textContent = "Delete";
+        initializeProjectDeleteButton(deleteButton, project, ProjectsController);
+
         const listItem = document.createElement("button");
-        listItem.classList.add("list-group-item", "list-group-item-action", "rounded-0", "border-end-0", "border-top-0");
+        listItem.classList.add("list-group-item", "list-group-item-action", "rounded-0", "border-end-0", "border-top-0", "d-flex", "justify-content-between");
         listItem.textContent = project.getTitle();
         listItem.addEventListener("click", (e) => {
             deactivateProjectsListItems();
             e.target.classList.add("active");
             setProjectDisplay(project);
         });
+
+        listItem.appendChild(deleteButton);
         list.appendChild(listItem);
     });
+}
+
+function initializeProjectDeleteButton(button, project, projectsController) {
+    button.addEventListener("click", (e) => {
+        e.stopPropagation();
+        const projectDisplay = document.querySelector("#project-display");
+        projectsController.removeProject(project);
+        populateProjectsList();
+        setProjectDisplayDefault(projectDisplay);
+    });
+}
+
+function setProjectDisplayDefault(display) {
+    const container = document.createElement("div");
+    container.classList.add("d-flex", "flex-column", "justify-content-center", "h-100");
+
+    const heading = document.createElement("h2");
+    heading.classList.add("fs-1", "w-100", "p-3", "text-center");
+    heading.textContent = "No project currently selected";
+
+    const subHeading = document.createElement("h3");
+    subHeading.classList.add("fs-2", "w-100", "p-3", "text-center", "text-secondary", "fw-light");
+    subHeading.textContent = "Click on a project in the list to the left to see all of the tasks associated with that project.";
+
+    display.innerHTML = "";
+    container.appendChild(heading);
+    container.appendChild(subHeading);
+    display.appendChild(container);
 }
 
 function clearProjectsList(list) {
