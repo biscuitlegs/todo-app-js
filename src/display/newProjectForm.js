63ebf66295e2526770defe1a988caf6d6c09467b
Bradley <bmcguigandev@outlook.com>
2021-07-05 04:15:59 +0100

    Update new project form styling

--- src/display/newProjectForm.js
+++ src/display/newProjectForm.js
@@ -1,18 +1,23 @@
 const form = document.createElement("div");
-form.classList.add("m-3", "collapse", "mb-4");
+form.classList.add("m-3", "collapse", "mb-4", "p-3", "border");
 form.setAttribute("id", "new-project-form");
+
+const titleLabel = document.createElement("label");
+titleLabel.setAttribute("for", "new-project-title");
+titleLabel.classList.add("form-label");
+titleLabel.textContent = "Title";
 
 const title = document.createElement("input");
 title.classList.add("form-control");
 title.setAttribute("type", "text");
 title.setAttribute("id", "new-project-title");
-title.setAttribute("placeholder", "Project Title...");
 
 const submit = document.createElement("button");
 submit.classList.add("btn", "btn-success", "w-100", "form-control", "my-2");
 submit.setAttribute("id", "new-project-submit");
 submit.textContent = "Add";
 
+form.appendChild(titleLabel);
 form.appendChild(title);
 form.appendChild(submit);
 
